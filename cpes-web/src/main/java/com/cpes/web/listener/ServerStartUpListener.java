0bb33885bf98b632e6d04e6729e1ab40c38c1b0d
JavaShiLong <shilong.jin@outlook.com>
2019-01-14 11:43:10 +0900

    cert_freemarker_finish

--- cpes-web/src/main/java/com/cpes/web/listener/ServerStartUpListener.java
+++ cpes-web/src/main/java/com/cpes/web/listener/ServerStartUpListener.java
@@ -3,7 +3,11 @@ package com.cpes.web.listener;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.cpes.service.PermissionService;
 
 public class ServerStartUpListener extends ContextLoaderListener {
 
@@ -17,6 +21,9 @@ public class ServerStartUpListener extends ContextLoaderListener {
 		application.setAttribute("APP_PATH", path);
 		
 		super.contextInitialized(event);
+		
+		
+		
 	}
 
 	
